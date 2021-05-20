b260f7812bc24a112025a6fd69f011ff1e249904
shawqihatem <shawqihatem@outlook.com>
2021-05-20 13:12:16 +0300

    Add Notifications Projects

---
+++ Notifications-Projects/NotificationOld/app/src/main/java/com/example/notificationold/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.notificationold
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        val id = 2222;
+        notificationButton.setOnClickListener {
+            val notification = NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle("My Notification")
+                .setContentText("This is a notification")
+                .build();
+            val myNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager;
+            myNotificationManager.notify(id, notification);
+        }
+    }
+}
